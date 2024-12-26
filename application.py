6542df79160f9153ae182aa1dba01b41460f54de
Jinny Kim <60960835+jiinkim-maker@users.noreply.github.com>
2024-12-27 04:03:09 +0900

    Update application.py

--- application.py
+++ application.py
@@ -52,9 +52,7 @@ st.write("아래에 문장을 입력하고, 맞춤법 교정을 확인하세요.
 # 사용자로부터 문장 입력 받기
 text_input = st.text_input("검사할 문장을 입력하세요:")
 
-if st.sidebar.button("문서 전체 맞춤법 검사 요청"):
-    uploaded_file = st.sidebar.file_uploader("문서를 업로드하세요", type=["docx", "pdf", "txt"])=
-
+st.sidebar.button("문서 전체 맞춤법 검사 요청")
 # 문서 업로드
 uploaded_file = st.sidebar.file_uploader("문서를 업로드하세요", type=["docx", "pdf", "txt"])
 
